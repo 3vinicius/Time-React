81439699883c019aa5ae6b341cd06c193c07ace6
3vinicius <amorimvinicius3@gmail.com>
2023-04-10 15:33:25 -0300

    add asiders texts

--- Timer-React/src/Components/Clock/index.jsx
+++ Timer-React/src/Components/Clock/index.jsx
@@ -4,10 +4,22 @@ export function Clock({days,hours,minutes,secundes}) {
   return(
   <section className='section-Clock'>
   <div className="container-Clock">
-    <div className='div-Clock'>{days}</div>
-    <div className='div-Clock'>{hours}</div>
-    <div className='div-Clock'>{minutes}</div>
-    <div className='div-Clock'>{secundes}</div>
+    <div className='div-Clock'>
+      <p className='number'>{days}</p>
+      <p className='bottomText'>Days</p>
+      </div>
+    <div className='div-Clock'>
+      <p className='number'>{hours}</p>
+      <p className='bottomText'>Hours</p>
+      </div>
+    <div className='div-Clock'>
+      <p className='number'>{minutes}</p>
+      <p className='bottomText'>Minutes</p>
+      </div>
+    <div className='div-Clock'>
+      <p className='number'>{secundes}</p>
+      <p className='bottomText'>Seconds</p>
+      </div>
   </div>
   </section>
   )
